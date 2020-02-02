86e372f5713aae3a45cbe7fbb5a05907770fa500
WrexialMT <dylan.abela@gmail.com>
2020-02-02 14:36:01 +0100

    Added pow

---
+++ Assets/1.Scripts/DestroyAfter.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class DestroyAfter : MonoBehaviour
+{
+    float timer = 0f;
+    public float duration = 0.25f;
+    public float maxScale = 0.1f;
+    private Vector3 originalScale;
+    private void Awake()
+    {
+        originalScale = transform.localScale;
+        transform.localScale = Vector3.zero;
+    }
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        var scaleDelta = Mathf.Clamp01(timer / maxScale);
+        transform.localScale = Vector3.Lerp(Vector3.zero, originalScale, scaleDelta);
+
+        if (timer > duration)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
